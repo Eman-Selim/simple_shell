c08c69de02b746c97e33fc2ec7a1ac14f2f1bfd1
Eman-Selim <87757692+Eman-Selim@users.noreply.github.com>
2023-05-18 16:56:17 +0300

    Update getenv.c

---
+++ getenv.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ * get_environ - the function that is responsible for returning the array of the environment
+ * @info: the potential argument that saved in structure
+ * Return: zero
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - this function is responsible for removing the env var
+ * @info: the potential argument that saved in structure
+ *  Return: zero or one
+ * @var: the variable of environment
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - the function is responsible for seting or editing env vars
+ * @info: the potential argument that saved in structure
+ * @var: the variable of environment
+ * @value: the value of the variable
+ *  Return: zero
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
