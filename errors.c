32d6589ebf396d7590b2e98c98470bd7da358de8
Eman-Selim <87757692+Eman-Selim@users.noreply.github.com>
2023-05-18 13:49:55 +0300

    Update errors.c

---
+++ errors.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+/**
+ *_eputs - function that responsible for printing the string
+ * @str: the input string
+ * Return: 
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - function that responsible for printing the characting
+ * @c: the input character
+ * Return: zero or one
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - function that responsible for writing c to the file descriptor
+ * @c: the input character
+ * @fd: the inputed file descriptor
+ * Return: zero or one
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - function that responsible for printing the inputted string
+ * @str: the parameter that contained the string
+ * @fd: the inputed file descriptor
+ * Return: the counted chars in the string
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
