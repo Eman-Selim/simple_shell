560d9e82f3ae1e15ff7be98fa701fb66727225dd
Eman-Selim <87757692+Eman-Selim@users.noreply.github.com>
2023-05-25 02:46:39 +0300

    Update string1.c

---
+++ string1.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ * _strcpy - this method is responsible for copying
+ * @dest: the place to save in
+ * @src: thevalue to save
+ * Return: address
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - this method is responsible for duplicating a tr
+ * @str: str hat reqiured to duplicate
+ * Return: address
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - method that responsible for printing inputs
+ *@str: input that reqiured to be printed
+ * Return: no returning
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - his method is reponsible for writing char inthe cconsol
+ * @c: The char that will be puted in output
+ * Return: one or minus one or int
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
