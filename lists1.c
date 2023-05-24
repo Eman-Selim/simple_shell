f0128ab9997b45db071c1275113feffb179ee332
Eman-Selim <87757692+Eman-Selim@users.noreply.github.com>
2023-05-25 00:15:16 +0300

    Update lists1.c

---
+++ lists1.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+/**
+ * list_len - this method is responsible for determining the list legnth
+ * @h: this is the address that pointed to the head
+ * Return: the list large
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * list_to_strings - this method is responsibl for returning the list data as array
+ * @head: this is the address that pointed to the head
+ * Return: the list large
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+
+/**
+ * print_list - this method is responsible for printing all list data
+ * @h: this is the address that pointed to the head
+ * Return: the list large
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * node_starts_with - this method is responsible for returning node with given data
+ * @node: this is the address that pointed to the head
+ * @prefix: data to compare with
+ * @c: the char that follow the given data
+ * Return: the node matched with th data
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - this method is responsible for geting the node num
+ * @head: this is the address that pointed to the head
+ * @node: the node whose number is required
+ * Return: minus one or the number
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
